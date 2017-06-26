aa9664097a65291858d95bf6b508141d41807ea5
Artem Barhanov <darakon92@gmail.com>
2017-06-26 22:52:44 +0300

    Refresh readme.md

--- main.go
+++ main.go
@@ -47,10 +47,3 @@ type Receipt struct {
 	Price float32 `json:"Price"`
 	Bill  string  `json:"Bill"`
 }
-
-func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
